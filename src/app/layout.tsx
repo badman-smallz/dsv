a7b2f72cba0bbc0245a5683136764cf574a7916b
Bhadmann Concepts <106613567+badman-smallz@users.noreply.github.com>
2025-10-23 10:24:50 +0100

    Installed HubSpot chatbot tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
           <Toaster />
         </AuthProvider>
 
-        
+        <!-- Start of HubSpot Embed Code -->
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/146872740.js"></script>
+        <!-- End of HubSpot Embed Code -->
   
       </body>
     </html>
